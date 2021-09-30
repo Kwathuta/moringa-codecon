4d796bc8995755b7f4c3648d528f3e6ace308658
ken <ken.mbira@student.moringaschool.com>
2021-09-30 20:31:20 +0300

    add service to make post request

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/user';
 
+import { UserServiceService } from 'src/app/services/user-service.service';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -8,8 +10,12 @@ import { User } from 'src/app/user';
 })
 export class SignupComponent implements OnInit {
   user = new User(0,"","","","");
+  addUser(){
+    this.userService.usermaker(this.user)
+    .subscribe()
+    }
 
-  constructor() { }
+  constructor(private userService:UserServiceService) { }
 
   ngOnInit(): void {
   }
