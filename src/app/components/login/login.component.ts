799fd6e1a65717777799aa3fa846ed6f73a9532d
ken <ken.mbira@student.moringaschool.com>
2021-09-30 12:23:01 +0300

    add validators for user login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from 'src/app/user';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  user = new User(0,"","","","")
   constructor() { }
 
   ngOnInit(): void {
