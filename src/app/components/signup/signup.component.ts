bca50dca4d1b73dbbbe4fbd13d62a2c712c08552
ken <ken.mbira@student.moringaschool.com>
2021-09-30 11:32:39 +0300

    add a user class

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/user';
 
 @Component({
   selector: 'app-signup',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  user = User;
 
   constructor() { }
 
